f0dff193d44196672380bb58a7aeb03fa2693c6b
nawwardaraghmeh <daraghmehnawwar@gmail.com>
2026-05-15 15:37:53 +0200

    fixed exit button functionality

--- Assets/Scripts/ExitButton.cs
+++ Assets/Scripts/ExitButton.cs
@@ -5,6 +5,11 @@ public class ExitButton : MonoBehaviour
     public void ExitGame()
     {
         Time.timeScale = 1f;
+        
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
         Application.Quit();
+        #endif
     }
 }
